c1b9e0967d3ea108aefb2b158e3bad408e3a1825
madyanes <madyanes.1995@gmail.com>
2023-01-28 12:56:54 +0700

    create user serializers

---
+++ backend/accounts/renderers.py
@@ -0,0 +1,31 @@
+import json
+from typing import Any, Mapping, Optional
+
+from rest_framework.renderers import JSONRenderer
+
+
+class UserJSONRenderer(JSONRenderer):
+
+    """Custom method."""
+
+    charset = 'utf-8'
+
+    def render(
+        self,
+        data: dict[str, Any],
+        media_type: Optional[str] = None,
+        renderer_context: Optional[Mapping[str, Any]] = None,
+    ) -> str:
+        """Returns a well formatted user JSON."""
+        errors = data.get('errors', None)
+        token = data.get('token', None)
+        if errors is not None:
+            return super(UserJSONRenderer, self).render(data)
+
+        if token is not None and isinstance(token, bytes):
+            # Also as mentioned above, we will decode `token` if it is of type
+            # bytes.
+            data['token'] = token.decode('utf-8')
+
+        # Finally, we can render out data under the "user" namespace.
+        return json.dumps({'user': data}) 
